3edb95c9c2d080827497d0e40d94029cc42e6d35
Divyansh chaudhary <hypr123890@gmail.com>
2025-04-27 05:18:20 +0530

    Add functionality to count words starting with a specific prefix

--- 7)Array/05.js
+++ 7)Array/05.js
@@ -90,3 +90,15 @@
 // }
 
 // console.log(newStr);
+
+let words = ["pay","attention","practice","attend"]
+let pref = "at"
+let count = 0;
+
+for(let i=0; i<words.length; i++){
+    let word = words[i];
+    // if(word.startsWith(pref)) count++;
+    if(word.substring(0,2) === pref) count++;
+}
+
+console.log(count);
