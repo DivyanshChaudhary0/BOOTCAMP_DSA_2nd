191f794434ae26abbd7eb7b9ecc6f8493780f97e
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-06 22:15:29 +0530

    Refactor: Implement funnyString function to determine if a string is funny based on character differences

--- 7)Array/tempCodeRunnerFile.js
+++ 7)Array/tempCodeRunnerFile.js
@@ -1,1 +1,16 @@
 
+function funnyString(s) {
+    // Reverse the string manually
+    let rev = s.split('').reverse().join('');
+    
+    for (let i = 1; i < s.length; i++) {
+        let originalDiff = Math.abs(s.charCodeAt(i) - s.charCodeAt(i - 1));
+        let reverseDiff = Math.abs(rev.charCodeAt(i) - rev.charCodeAt(i - 1));
+        
+        if (originalDiff !== reverseDiff) {
+            return "Not Funny";
+        }
+    }
+
+    return "Funny";
+}
