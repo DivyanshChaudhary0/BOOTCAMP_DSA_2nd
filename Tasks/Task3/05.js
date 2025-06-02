7ee705dbc6d02cb4848c757e9eb81b062a161388
Divyansh chaudhary <hypr123890@gmail.com>
2025-06-02 21:43:01 +0530

    Refactor: Add initial implementation of asynchronous file reading with callbacks

---
+++ Tasks/Task3/05.js
@@ -0,0 +1,22 @@
+
+const fs = require("fs");
+
+setImmediate(() => console.log("Immediate 1"));
+
+setTimeout(() => console.log("Timeout 1"),0);
+
+Promise.resolve("Promise resolved").then(console.log);
+
+fs.readFile("./Tasks/Task3/hello.text","utf-8", () => {
+    setTimeout(() => console.log("Timeout 2"),0);
+    
+    process.nextTick(() => console.log("next tick 2"));
+
+    setImmediate(() => console.log("Immediate 1"));
+    
+    console.log("File read CB");
+})
+
+process.nextTick(() => console.log("next tick 1"));
+
+console.log("Last line");
