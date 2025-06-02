ea5acba541cc4b87ebaf1db2b8cb47bbf7a44007
Divyansh chaudhary <hypr123890@gmail.com>
2025-06-02 21:43:46 +0530

    Refactor: Clean up code structure by organizing asynchronous calls and removing duplicates

--- Tasks/Task3/05.js
+++ Tasks/Task3/05.js
@@ -1,6 +1,21 @@
 
 const fs = require("fs");
 
+
+setImmediate(() => console.log("Immediate 1"));
+
+setTimeout(() => console.log("Timeout 1"),0);
+
+Promise.resolve("Promise resolved").then(console.log);
+
+fs.readFile("./Tasks/Task3/hello.text","utf-8", () => {
+    console.log("File read CB");
+})
+
+process.nextTick(() => console.log("next tick 1"));
+
+
+
 setImmediate(() => console.log("Immediate 1"));
 
 setTimeout(() => console.log("Timeout 1"),0);
