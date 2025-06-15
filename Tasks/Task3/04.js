49c9447590b2bc153d9d074a209af22bfc9e7fa1
Divyansh chaudhary <hypr123890@gmail.com>
2025-06-15 08:07:42 +0530

    Refactor: Comment out unused asynchronous function and related calls

--- Tasks/Task3/04.js
+++ Tasks/Task3/04.js
@@ -1,7 +1,24 @@
 
-async function foo(){
-    return "hello world"
+// async function foo(){
+//     return "hello world"
+// }
+
+// const result = await foo()
+// console.log(result);
+
+
+let str = 'hello world kya ho rha hai lodu'
+
+function makeFirstUpperCase(str){
+    let strArray = str.split(" ");
+    let newStr = '';
+
+    for(let i=0; i<strArray.length; i++){
+        let char = strArray[i];
+        newStr += char[0].toUpperCase() + char.slice(1) + " ";
+    }
+    
+    return newStr;
 }
 
-const result = await foo()
-console.log(result);
+console.log(makeFirstUpperCase(str));
