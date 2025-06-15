58f8ad330dfe3b1ea938dbe45a28003f6fdc3223
Divyansh chaudhary <hypr123890@gmail.com>
2025-06-15 07:59:40 +0530

    Refactor: Remove duplicate asynchronous calls for improved clarity

--- Tasks/Task3/05.js
+++ Tasks/Task3/05.js
@@ -1,7 +1,6 @@
 
 const fs = require("fs");
 
-
 setImmediate(() => console.log("Immediate 1"));
 
 setTimeout(() => console.log("Timeout 1"),0);
@@ -13,7 +12,6 @@ fs.readFile("./Tasks/Task3/hello.text","utf-8", () => {
 })
 
 process.nextTick(() => console.log("next tick 1"));
-
 
 
 setImmediate(() => console.log("Immediate 1"));
